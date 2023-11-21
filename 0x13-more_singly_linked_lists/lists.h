4c7f7b7a53260fbbaaea0af0fe5cf75083d582c0
oluwanikhnyur <oluwanikhnyur@gmail.com>
2023-11-21 12:37:18 +0100

    Corrections

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H_
 #define LISTS_H_
+
 #include <stdlib.h>
+#include <stdlio>
 
 /**
 * struct listint_s - singly linked list
@@ -32,4 +34,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif
+#endif /*LINKED*/
