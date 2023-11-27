8b14e880094cc1e67d641cd2f5ecbf7b15c2e783
oluwanikhnyur <oluwanikhnyur@gmail.com>
2023-11-27 23:32:05 +0100

    Tasl

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Appends text to the end of a file
+ * @filename: The name of the file.
+ * @text_content: The content added.
+ *
+ * Return: Return 1 if the file exists and return -1 if the file doesn't exist or if it fails
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file_d;
+	int nletters;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+
+	file_d = open (filename, O_WRONLY | O_APPEND);
+
+	if (file_d == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (nletters = 0; text_content [nletters]; nletters++)
+			;
+
+		rwr = write (file_d, text_content, nletters);
+
+		if (rwr == -1)
+			return (-1);
+	}
+
+	close (file_d);
+
+	return (1);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+This directory contains solutions to the 0x15 File io tasks. Done this day, 27th of November, around 7:49 p.m WAT
