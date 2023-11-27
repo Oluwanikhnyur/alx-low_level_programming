68999464a89af1bd2fd38638c392915747530fb7
oluwanikhnyur <oluwanikhnyur@gmail.com>
2023-11-27 15:02:21 -0800

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,6 @@
 * Return: The actual number of letters it could read and
 * as the same time print
 */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file_d;
@@ -25,25 +24,32 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
+
 	file_d = open(filename, O_RDONLY);
 	if (file_d == -1)
 		return (0);
+
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
-		close(file_d);
+	close(file_d);
 		return (0);
 	}
+
 	lenr = read(file_d, buffer, letters);
 	close(file_d);
+
 	if (lenr == -1)
 	{
-		free(buffer);
+	free(buffer);
 		return (0);
 	}
+
 	lenw = write(STDOUT_FILENO, buffer, lenr);
 	free(buffer);
-	if (lenr != lenw)
+
+	if (lenw == -1 || lenw != lenr)
 		return (0);
+
 	return (lenw);
 }
