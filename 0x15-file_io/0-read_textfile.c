c429aa92cf5109918a0ebc8c6fec8696d7b2fa12
oluwanikhnyur <oluwanikhnyur@gmail.com>
2023-11-27 15:04:33 -0800

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,50 +6,39 @@
 #include <stdlib.h>
 
 /**
-* read_textfile - This will read a text file and as well
-* prints the file to the POSIX stdo
-*
-* @filename: The name of the file to be read
-*
-* @letters: The number of letters it should read and as well print
-*
-* Return: The actual number of letters it could read and
-* as the same time print
-*/
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_d;
+	int fd;
 	ssize_t lenr, lenw;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
-
-	file_d = open(filename, O_RDONLY);
-	if (file_d == -1)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
-	close(file_d);
+		close(fd);
 		return (0);
 	}
-
-	lenr = read(file_d, buffer, letters);
-	close(file_d);
-
+	lenr = read(fd, buffer, letters);
+	close(fd);
 	if (lenr == -1)
 	{
-	free(buffer);
+		free(buffer);
 		return (0);
 	}
-
 	lenw = write(STDOUT_FILENO, buffer, lenr);
 	free(buffer);
-
-	if (lenw == -1 || lenw != lenr)
+	if (lenr != lenw)
 		return (0);
-
 	return (lenw);
 }
